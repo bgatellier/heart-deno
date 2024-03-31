c204a50eb16bf2d5ad7ec481be3351d2f9b855c3
Bastien Gatellier <contact@bgatellier.fr>
2024-03-31 19:17:14 +0200

    feat: add ssllabs package (#9)

---
+++ packages/ssllabs-server/src/api/Error.ts
@@ -0,0 +1,14 @@
+/**
+ * @see [Documentation]{@link https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md#error-reporting}
+ */
+export interface SsllabsServerError {
+  errors: {
+    field: string;
+    message: string;
+  }[];
+}
+
+export const isSsllabsServerError = (
+  object: unknown,
+): object is SsllabsServerError =>
+  Array.isArray((object as SsllabsServerError).errors);
